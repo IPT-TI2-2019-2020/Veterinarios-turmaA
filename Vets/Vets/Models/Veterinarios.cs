28e06faab0715788d1c3b5ffea6f548027689b04
Jose Casimiro Nunes Pereira <casimiro@ipt.pt>
2020-04-02 11:20:54 +0100

    adição das classes do Modelo, representando os atributos 'naturais' de cada entidade

---
+++ Vets/Vets/Models/Veterinarios.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Vets.Models {
+   public class Veterinarios {
+
+      // create table Veterinarios(
+      //   ID integer Primary Key autoincrement,
+      //   Nome varchar(60) not null,
+      //   .....
+      // )
+
+     public int ID { get; set; }
+
+      public string Nome{ get; set; }
+
+      public string NumCedulaProf { get; set; }
+
+      public string Fotografia { get; set; }
+
+
+
+   }
+}
