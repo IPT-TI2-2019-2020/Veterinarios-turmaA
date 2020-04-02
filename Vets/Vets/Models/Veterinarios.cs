778474ee2ab0d0717f3cd7d42e2381b4d35e8dd3
Jose Casimiro Nunes Pereira <casimiro@ipt.pt>
2020-04-02 12:19:14 +0100

    Especificação dos 'relacionamentos' entre as 'entidades', representadas pelas classes

--- Vets/Vets/Models/Veterinarios.cs
+++ Vets/Vets/Models/Veterinarios.cs
@@ -1,10 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Vets.Models {
    public class Veterinarios {
+
+
+      public Veterinarios() {
+         ListaConsultas = new HashSet<Consultas>();
+      }
+
+
 
       // create table Veterinarios(
       //   ID integer Primary Key autoincrement,
@@ -12,15 +20,17 @@ namespace Vets.Models {
       //   .....
       // )
 
-     public int ID { get; set; }
+      [Key]
+      public int ID { get; set; }
 
-      public string Nome{ get; set; }
+      public string Nome { get; set; }
 
       public string NumCedulaProf { get; set; }
 
       public string Fotografia { get; set; }
 
-
+      //*********************************************
+      public ICollection<Consultas> ListaConsultas { get; set; }
 
    }
 }
