0a445f66297a261788f5c8d461b415f9926840b7
Jose Casimiro Nunes Pereira <casimiro@ipt.pt>
2020-04-30 12:30:26 +0100

    Adição de 'anotadores' ao modelo dos Veterinários + Configuração da interface dos Veterinários (correção da língua + melhoramentos vários)

--- Vets-tA/Vets/Controllers/VeterinariosController.cs
+++ Vets-tA/Vets/Controllers/VeterinariosController.cs
@@ -8,146 +8,130 @@ using Microsoft.EntityFrameworkCore;
 using Vets.Data;
 using Vets.Models;
 
-namespace Vets.Controllers
-{
-    public class VeterinariosController : Controller
-    {
-        private readonly VetsDB _context;
+namespace Vets.Controllers {
+   public class VeterinariosController : Controller {
+      private readonly VetsDB _context;
 
-        public VeterinariosController(VetsDB context)
-        {
-            _context = context;
-        }
+      public VeterinariosController(VetsDB context) {
+         _context = context;
+      }
 
-        // GET: Veterinarios
-        public async Task<IActionResult> Index()
-        {
-            return View(await _context.Veterinarios.ToListAsync());
-        }
 
-        // GET: Veterinarios/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
 
-            var veterinarios = await _context.Veterinarios
-                .FirstOrDefaultAsync(m => m.ID == id);
-            if (veterinarios == null)
-            {
-                return NotFound();
-            }
+      // GET: Veterinarios
+      public async Task<IActionResult> Index() {
+         return View(await _context.Veterinarios.ToListAsync());
+      }
 
-            return View(veterinarios);
-        }
 
-        // GET: Veterinarios/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
 
-        // POST: Veterinarios/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("ID,Nome,NumCedulaProf,Fotografia")] Veterinarios veterinarios)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(veterinarios);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(veterinarios);
-        }
 
-        // GET: Veterinarios/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+      // GET: Veterinarios/Details/5
+      public async Task<IActionResult> Details(int? id) {
+         if (id == null) {
+            return NotFound();
+         }
 
-            var veterinarios = await _context.Veterinarios.FindAsync(id);
-            if (veterinarios == null)
-            {
-                return NotFound();
-            }
-            return View(veterinarios);
-        }
+         var veterinarios = await _context.Veterinarios.FirstOrDefaultAsync(m => m.ID == id);
 
-        // POST: Veterinarios/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("ID,Nome,NumCedulaProf,Fotografia")] Veterinarios veterinarios)
-        {
-            if (id != veterinarios.ID)
-            {
-                return NotFound();
-            }
+         if (veterinarios == null) {
+            return NotFound();
+         }
 
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(veterinarios);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!VeterinariosExists(veterinarios.ID))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(veterinarios);
-        }
+         return View(veterinarios);
+      }
 
-        // GET: Veterinarios/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
 
-            var veterinarios = await _context.Veterinarios
-                .FirstOrDefaultAsync(m => m.ID == id);
-            if (veterinarios == null)
-            {
-                return NotFound();
-            }
 
-            return View(veterinarios);
-        }
 
-        // POST: Veterinarios/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var veterinarios = await _context.Veterinarios.FindAsync(id);
-            _context.Veterinarios.Remove(veterinarios);
+
+      // GET: Veterinarios/Create
+      public IActionResult Create() {
+         return View();
+      }
+
+      // POST: Veterinarios/Create
+      // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+      // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+      [HttpPost]
+      [ValidateAntiForgeryToken]
+      public async Task<IActionResult> Create([Bind("ID,Nome,NumCedulaProf,Fotografia")] Veterinarios veterinarios) {
+         if (ModelState.IsValid) {
+            _context.Add(veterinarios);
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
-        }
+         }
+         return View(veterinarios);
+      }
 
-        private bool VeterinariosExists(int id)
-        {
-            return _context.Veterinarios.Any(e => e.ID == id);
-        }
-    }
+      // GET: Veterinarios/Edit/5
+      public async Task<IActionResult> Edit(int? id) {
+         if (id == null) {
+            return NotFound();
+         }
+
+         var veterinarios = await _context.Veterinarios.FindAsync(id);
+         if (veterinarios == null) {
+            return NotFound();
+         }
+         return View(veterinarios);
+      }
+
+      // POST: Veterinarios/Edit/5
+      // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+      // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+      [HttpPost]
+      [ValidateAntiForgeryToken]
+      public async Task<IActionResult> Edit(int id, [Bind("ID,Nome,NumCedulaProf,Fotografia")] Veterinarios veterinarios) {
+         if (id != veterinarios.ID) {
+            return NotFound();
+         }
+
+         if (ModelState.IsValid) {
+            try {
+               _context.Update(veterinarios);
+               await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException) {
+               if (!VeterinariosExists(veterinarios.ID)) {
+                  return NotFound();
+               }
+               else {
+                  throw;
+               }
+            }
+            return RedirectToAction(nameof(Index));
+         }
+         return View(veterinarios);
+      }
+
+      // GET: Veterinarios/Delete/5
+      public async Task<IActionResult> Delete(int? id) {
+         if (id == null) {
+            return NotFound();
+         }
+
+         var veterinarios = await _context.Veterinarios
+             .FirstOrDefaultAsync(m => m.ID == id);
+         if (veterinarios == null) {
+            return NotFound();
+         }
+
+         return View(veterinarios);
+      }
+
+      // POST: Veterinarios/Delete/5
+      [HttpPost, ActionName("Delete")]
+      [ValidateAntiForgeryToken]
+      public async Task<IActionResult> DeleteConfirmed(int id) {
+         var veterinarios = await _context.Veterinarios.FindAsync(id);
+         _context.Veterinarios.Remove(veterinarios);
+         await _context.SaveChangesAsync();
+         return RedirectToAction(nameof(Index));
+      }
+
+      private bool VeterinariosExists(int id) {
+         return _context.Veterinarios.Any(e => e.ID == id);
+      }
+   }
 }
